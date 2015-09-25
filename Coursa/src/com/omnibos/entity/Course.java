df21218906b48e18729fe490f51004fdad4d3df8
wujm <14302010011@fudan.edu.cn>
2015-09-25 11:01:34 +0800

    entity revised

--- Coursa/src/com/omnibos/entity/Course.java
+++ Coursa/src/com/omnibos/entity/Course.java
@@ -1,7 +1,7 @@
 package com.omnibos.entity;
 
 public class Course {
-	
+
 	private String moduleId;
 	private String courseId;
 	private String courseName;
@@ -10,88 +10,114 @@ public class Course {
 	private String post;
 	private String stdNumLimit;
 	private String location;
-	private String duration;
+	private String time;
 	private String extraInfo;
 	private String examInfo;
 	private String classBelong;
-	
-	
-	
+	private String restriction;
 
 	public String getModuleId() {
 		return moduleId;
 	}
+
 	public void setModuleId(String moduleId) {
 		this.moduleId = moduleId;
 	}
+
 	public String getCourseId() {
 		return courseId;
 	}
+
 	public void setCourseId(String courseId) {
 		this.courseId = courseId;
 	}
+
 	public String getCourseName() {
 		return courseName;
 	}
+
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
+
 	public String getCredit() {
 		return credit;
 	}
+
 	public void setCredit(String credit) {
 		this.credit = credit;
 	}
+
 	public String getTutors() {
 		return tutors;
 	}
+
 	public void setTutors(String tutors) {
 		this.tutors = tutors;
 	}
+
 	public String getPost() {
 		return post;
 	}
+
 	public void setPost(String post) {
 		this.post = post;
 	}
+
 	public String getStdNumLimit() {
 		return stdNumLimit;
 	}
+
 	public void setStdNumLimit(String stdNumLimit) {
 		this.stdNumLimit = stdNumLimit;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
-	public String getDuration() {
-		return duration;
+
+	public String getTime() {
+		return time;
 	}
-	public void setDuration(String duration) {
-		this.duration = duration;
+
+	public void setTime(String time) {
+		this.time = time;
 	}
+
 	public String getExtraInfo() {
 		return extraInfo;
 	}
+
 	public void setExtraInfo(String extraInfo) {
 		this.extraInfo = extraInfo;
 	}
+
 	public String getExamInfo() {
 		return examInfo;
 	}
+
 	public void setExamInfo(String examInfo) {
 		this.examInfo = examInfo;
 	}
+
 	public String getClassBelong() {
 		return classBelong;
 	}
+
 	public void setClassBelong(String classBelong) {
 		this.classBelong = classBelong;
 	}
 
-	
-	
-	
+	public String getRestriction() {
+		return restriction;
+	}
+
+	public void setRestriction(String restriction) {
+		this.restriction = restriction;
+	}
+
 }
